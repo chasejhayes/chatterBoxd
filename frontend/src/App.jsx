f854a3fb54920b0dd4e855369a2db55a505acd20
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-08-12 13:34:53 -0400

    Select a film to open a popup dialog

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,22 +24,20 @@ import './style.css'
     </div>
   )
 
-  // At some point, the hardcoded userMovies is being wiped and replaced by the single added film from the dropdown
+
 
   const MovieDisplay = ({userMovies}) => {
-    console.log(userMovies)
-    
     return (
     <div>
       <ul>
         {userMovies.map((item) => 
-        <li>
+        <li key={item.id}>
           <h2>{item.title}</h2>
-          {/* <p>Directed By: {item.director}</p>
+          <p>Directed By: {item.director}</p>
           <h3>Description</h3>
           <p>{item.description}</p>
           <p>Average Rating: {item.averageRating}</p>
-          <p>Reviews: {item.reviews}</p> */}
+          <p>Reviews: {item.reviews}</p>
         </li>
         )}
       </ul>
@@ -61,22 +59,42 @@ import './style.css'
         </div>
   )
 
-  const Add_Films = ({movies, setUserMovies, userMovies}) => {
+  const Form_Popup = ({showForm}) => {
+    if (showForm === true)
+    return (
+      <div>
+        <div>IT WORKED</div>
+       <dialog open>
+          <p>Hello! This is your dialog.</p>
+          <button >Close</button>
+        </dialog>
+      </div>
+
+    )
+  }
+
+  // formpopup only runs when 'true', onChange needs to change state to true/false
+
+  const Add_Films = ({movies, setUserMovies, userMovies,showForm, setShowForm}) => {
 
     const handleAddFilms = (e) => {
       let id = Number(e.target.value);
       let newMovie = movies.find(movie => movie.id === id)
       return setUserMovies([...userMovies, newMovie])
     }
-
+    
+    const handleShowForm = () => {
+      return setShowForm(true)
+    }
 
     return (
       <div>
-        <select name="movies" id="movies" onChange={handleAddFilms}>
+        <select name="movies" id="movies" onChange={(e) => {handleAddFilms(e); handleShowForm()}}>
           {movies.map(item => 
-            <option value={item.id}>{item.title}</option>
+            <option key={item.id} value={item.id}>{item.title}</option>
           )}
         </select>
+        <Form_Popup showForm={showForm}/>
       </div>
     )
   }
@@ -85,13 +103,12 @@ import './style.css'
 function App() {
 
   const [movies, setMovies] = useState([]);
-  const [userMovies, setUserMovies] = useState([    {id:4, title: "Leave Her to Heaven", director: "John M. Stahl", releaseDate: 1945, description: "While on a train, writer Richard Harland (Cornel Wilde) strikes up a relationship with the gorgeous Ellen Berent (Gene Tierney). Ellen quickly becomes obsessed with Richard and abandons her fiancé, Russell Quinton (Vincent Price), to be with him. The couple rushes into marriage, with both of them caught up in romance and Richard intrigued by Ellen's intensity. Only after settling into marriage, however, does Richard realize that she is psychotically jealous and highly unstable.", averageRating: 0, reviews: []},
-    {id:5, title: "The Maltese Falcon", director: "John Huston", releaseDate: 1941, description: "In this noir classic, detective Sam Spade (Humphrey Bogart) gets more than he bargained for when he takes a case brought to him by a beautiful but secretive woman (Mary Astor). As soon as Miss Wonderly shows up, trouble follows as Sam's partner is murdered and Sam is accosted by a man (Peter Lorre) demanding he locate a valuable statuette. Sam, entangled in a dangerous web of crime and intrigue, soon realizes he must find the one thing they all seem to want: the bejeweled Maltese falcon.", averageRating: 0, reviews: []}])
+  const [userMovies, setUserMovies] = useState([])
+  const [showForm, setShowForm] = useState(false)
 
   useEffect(() => {
     axios.get('http://localhost:3001/api/movies')
       .then((response) => {
-        console.log(response.data)
         setMovies(response.data)
       })
       .catch((error) => {
@@ -108,7 +125,7 @@ function App() {
       <Header />
       <div id="body">
         <Profile_Header />
-        <Add_Films movies={movies} setUserMovies={setUserMovies} userMovies={userMovies}/>
+        <Add_Films movies={movies} setUserMovies={setUserMovies} userMovies={userMovies} showForm={showForm} setShowForm={setShowForm}/>
         <Body_Right userMovies={userMovies}/>
       </div>
     </div>
